78474ae3bed19fc8d6bf8ceb05dc76c1605e1315
Sri Krishna <skrishna@buf.build>
2023-05-15 20:22:03 +0530

    Add relation registration API

--- packages/knit-gateway/src/gateway.ts
+++ packages/knit-gateway/src/gateway.ts
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import type { Transport } from "@bufbuild/connect";
-import { MethodKind } from "@bufbuild/protobuf";
+import { MethodKind, type PlainMessage } from "@bufbuild/protobuf";
 import type {
   ServiceType,
   MethodInfo,
@@ -22,6 +22,7 @@ import type {
   MessageType,
   PartialMessage,
 } from "@bufbuild/protobuf";
+import type { RelationConfig } from "@buf/bufbuild_knit.bufbuild_es/buf/knit/v1alpha1/options_pb.js";
 
 /**
  * Options to configure {@link Gateway}
@@ -61,6 +62,17 @@ interface GatewayServiceOptions<S extends ServiceType> {
   timeoutMs?: number;
 }
 
+interface GatewayRelationOptions {
+  /**
+   * The transport to use for this relation. Defaults to the base transport of the gateway.
+   */
+  transport?: Transport;
+  /**
+   * The timeout in millisecond to use for this service.
+   */
+  timeoutMs?: number;
+}
+
 /**
  * Gateway holds the state of the KnitService
  *
@@ -84,6 +96,14 @@ export interface Gateway {
   addService<S extends ServiceType>(
     service: S,
     options?: GatewayServiceOptions<S>
+  ): void;
+  /**
+   * Add relation(s) to the gateway.
+   */
+  addRelation<S extends ServiceType>(
+    service: S,
+    methods: RelationMethodConfig<S>,
+    options?: GatewayRelationOptions
   ): void;
 }
 
@@ -204,5 +224,117 @@ export function createGateway({
         });
       }
     },
+    addRelation<S extends ServiceType>(
+      service: S,
+      methods: RelationMethodConfig<S>,
+      options?: GatewayRelationOptions
+    ) {
+      for (const [method, config] of Object.entries(methods)) {
+        if (config === undefined) {
+          throw new Error(
+            `Knit: ${method}: relation config cannot be undefined`
+          );
+        }
+        if (config.name === "") {
+          throw new Error("Knit: relation name cannot be empty");
+        }
+        const methodInfo = service.methods[method];
+        const base = getRepeatedMessage(methodInfo.I, 1, "bases");
+        if (
+          base.fields.list().find((f) => f.name === config.name) !== undefined
+        ) {
+          throw new Error(
+            `Knit: ${method}: relation name '${config.name}' already exists on ${base.typeName}`
+          );
+        }
+        const shell = getRepeatedMessage(methodInfo.O, 1, "values");
+        const shellFields = shell.fields.list();
+        if (shellFields.length !== 1) {
+          throw new Error(
+            `Knit: ${method}: relation must have exactly one field, found ${shellFields.length}`
+          );
+        }
+        const field = shellFields[0];
+        if (field.no !== 1) {
+          throw new Error(
+            `Knit: ${method}: relation ${field.name} must have tag 1, found ${field.no}`
+          );
+        }
+        if (field.name !== config.name) {
+          throw new Error(
+            `Knit: ${method}: relation field must be named '${config.name}', found '${field.name}'`
+          );
+        }
+        let baseRelations = relations.get(base.typeName);
+        if (baseRelations === undefined) {
+          baseRelations = new Map();
+          relations.set(base.typeName, baseRelations);
+        }
+        if (baseRelations.has(config.name)) {
+          throw new Error(
+            `Knit: ${method}: relation name '${config.name}' already exists on ${base.typeName}`
+          );
+        }
+        const paramFields = methodInfo.I.fields
+          .list()
+          .filter((f) => f.no !== 1);
+        let params: MessageType | undefined = undefined;
+        if (paramFields.length > 0) {
+          params = methodInfo.I.runtime.makeMessageType(
+            `buf.knit.params.${base.typeName}.${config.name}`,
+            methodInfo.I.fields.list().filter((f) => f.no !== 1)
+          );
+        }
+        baseRelations.set(config.name, {
+          base,
+          field,
+          runtime: shell.runtime,
+          params,
+          resolver: async (bases, params) => {
+            const resolverTransport = options?.transport ?? transport;
+            const resolverTimeoutMs = options?.timeoutMs ?? timeoutMs;
+            const response = await resolverTransport.unary(
+              service,
+              methodInfo,
+              undefined, // TODO: add cancelation support
+              resolverTimeoutMs,
+              undefined, // TODO: propagate headers
+              {
+                bases,
+                ...params,
+              }
+            );
+            return (response.message["values"] as Array<AnyMessage>).map(
+              (v: AnyMessage) => v[field.localName] as unknown
+            );
+          },
+        });
+      }
+    },
   };
 }
+
+function getRepeatedMessage(message: MessageType, tag: number, name: string) {
+  const field = message.fields.find(tag);
+  if (field === undefined) {
+    throw new Error(
+      `Knit: ${message.name}: relation must have a '${name}' field with tag ${tag}`
+    );
+  }
+  if (field.name !== name) {
+    throw new Error(
+      `Knit: ${message.name}: field with ${tag} must be named '${name}', found '${field.name}'`
+    );
+  }
+  if (!field.repeated) {
+    throw new Error(`Knit: ${message.name}: bases field must be repeated`);
+  }
+  if (field.kind !== "message") {
+    throw new Error(`Knit: ${message.name}: bases field must be a message `);
+  }
+  return field.T;
+}
+
+type RelationMethodConfig<S extends ServiceType> = {
+  [K in keyof S["methods"]]?: PlainMessage<RelationConfig>;
+};
